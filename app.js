2925718ff08ce5b8edbd7b3d052d109f2cf626ee
João Palmeiro <joaommpalmeiro@gmail.com>
2018-09-05 20:47:35 +0100

    Update: Heroku Procfile and Port.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const routes = require('./router');
 
 // Define the hostname and port where the server can be found
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
